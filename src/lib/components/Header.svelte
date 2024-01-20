849dc82afed74dbcff29f02cb92959137b6a11ee
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2024-01-20 20:06:41 +0100

    Start

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,154 @@
+<script>
+
+</script>
+
+<header>
+	<input type="checkbox" id="toggle" style="display:none;" />
+	<label class="toggle-btn toggle-btn__cross" for="toggle">
+	  <div class="bar"></div>
+	  <div class="bar"></div>
+	  <div class="bar"></div>
+	</label>
+	<nav>
+	  <ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/">About</a></li>
+		<li><a href="/">Promise</a></li>
+		<li><a href="/">Sponsor</a></li>
+		<li><a href="/">Contact</a></li>
+		<li><a href="/">FAQ</a></li>
+	  </ul>
+	</nav>
+	<div class="logo">
+		<img src="./inkheader.png" alt="INK" />
+	</div>
+  </header>  
+
+<style>
+ :root {
+	 font-size: 16px;
+}
+
+ * {
+	 box-sizing: border-box;
+	 font-family: var(--Apercu-Font);
+	 color: #555;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px; /* Adjust padding as needed */
+  /* Other styles for header */
+}
+
+.logo {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+}
+
+.logo img {
+  width: clamp(25px, 8vw, 65px);
+  height: auto;
+  /* Other styles for the logo image */
+}
+
+ header nav {
+	 background-color: rgba(0, 0, 0, .8);
+	 position: fixed;
+	 top: 0;
+	 left: 0;
+	 width: 100%;
+	 height: 100%;
+	 opacity: 0;
+	 visibility: hidden;
+	 transition: all 0.3s ease-in-out;
+}
+ header nav ul {
+	 list-style-type: none;
+	 padding: 0;
+	 margin: 0;
+	 position: absolute;
+	 left: 50%;
+	 top: 170px;
+	 transition: all 0.3s ease-in-out;
+	 transform: translateX(-50%);
+}
+ header nav ul li {
+	 transform: translateY(50px);
+	 opacity: 0;
+}
+ header nav ul li a {
+	 display: block;
+	 font-size: 2em;
+	 text-decoration: none;
+	 padding: 10px 0;
+	 text-align: center;
+	 color: #fff;
+	 font-weight: bold;
+	 transition: all 0.2s ease-in-out;
+	 text-shadow: 1px 1px 2px rgba(0, 0, 0, .5);
+}
+ header nav ul li a:hover {
+	 color: var(--candelLight);
+}
+ .toggle-btn {
+	 display: block;
+	 position: fixed;
+	 z-index: 10;
+	 right: 10px;
+	 top: 10px;
+	 cursor: pointer;
+}
+ .toggle-btn .bar {
+	 width: 30px;
+	 height: 2px;
+	 margin: 7px auto;
+	 background-color: #fff;
+	 transition: all 0.3s ease-in-out;
+	 box-shadow: 0 0 3px 1px rgba(0, 0, 0, .3);
+}
+ .toggle-btn .bar:nth-child(2) {
+	 width: 20px;
+}
+ #toggle:checked ~ nav {
+	 opacity: 1;
+	 visibility: visible;
+}
+ #toggle:checked ~ nav ul {
+	 top: 70px;
+}
+ #toggle:checked ~ nav ul li {
+	 transform: translateY(0px);
+	 opacity: 1;
+}
+ #toggle:checked ~ nav ul li:nth-child(1) {
+	 transition: all 0.3s cubic-bezier(0.6, 0, 0.8, 1.5) 0.1s;
+}
+ #toggle:checked ~ nav ul li:nth-child(2) {
+	 transition: all 0.3s cubic-bezier(0.6, 0, 0.8, 1.5) 0.2s;
+}
+ #toggle:checked ~ nav ul li:nth-child(3) {
+	 transition: all 0.3s cubic-bezier(0.6, 0, 0.8, 1.5) 0.3s;
+}
+ #toggle:checked ~ nav ul li:nth-child(4) {
+	 transition: all 0.3s cubic-bezier(0.6, 0, 0.8, 1.5) 0.4s;
+}
+ #toggle:checked + label.toggle-btn .bar {
+	 background-color: red;
+}
+ #toggle:checked + label.toggle-btn .bar:nth-child(2) {
+	 transform: translateX(50px);
+	 opacity: 0;
+}
+ #toggle:checked + label.toggle-btn .bar:nth-child(1) {
+	 transform: translateY(10px) rotate(45deg);
+}
+ #toggle:checked + label.toggle-btn .bar:nth-child(3) {
+	 transform: translateY(-8px) rotate(-45deg);
+}
+ 
+
+</style>
---
+++ src/routes/payment/+page.svelte
@@ -0,0 +1,203 @@
+<script>
+
+</script>
+
+<main>
+<div class="mainscreen">
+    <!-- <img src="https://image.freepik.com/free-vector/purple-background-with-neon-frame_52683-34124.jpg" class="bgimg" alt=""> -->
+    <div class="card">
+        <div class="leftside">
+            <img
+                src="/ink.png"
+                class="product"
+                alt="Shoes"
+            />
+        </div>
+        <div class="rightside">
+            <form action="">
+                <h1>CheckOut</h1>
+                <h2>Payment Information</h2>
+                <p>Cardholder Name</p>
+                <input type="text" class="inputbox" name="name" required />
+                <p>Donation Option</p>
+                <select class="inputbox" name="card_type" id="card_type" required>
+                    <option value="">--Select a option--</option>
+                    <option value="Visa">Material</option>
+                    <option value="RuPay">Financial</option>
+                    <option value="MasterCard">Clothing</option>
+                </select>
+
+                <p>Card Number</p>
+                <input type="number" class="inputbox" name="card_number" id="card_number" required />
+
+                <p>Card Type</p>
+                <select class="inputbox" name="card_type" id="card_type" required>
+                    <option value="">--Select a Card Type--</option>
+                    <option value="Visa">Visa</option>
+                    <option value="RuPay">Discovery</option>
+                    <option value="MasterCard">MasterCard</option>
+                </select>
+                <div class="expcvv">
+                    <p class="expcvv_text">Expiry</p>
+                    <input type="date" class="inputbox" name="exp_date" id="exp_date" required />
+                    <p class="expcvv_text2">CVV</p>
+                    <input type="cc-csc" class="inputbox" name="cvv" id="cvv" required />
+                </div>
+                <p>Amount</p>
+                <div style="display: flex;">
+                    <span style="margin-right: 5px;">$</span>
+                    <input type="number" class="inputbox" name="amount" id="amount" required />
+                </div>
+                <p></p>
+                <form action="/paymentgood" method="get">
+                    <button type="submit" class="button">
+                     CheckOut
+                    </button>
+                  </form>
+                  
+            </form>
+        </div>
+    </div>
+</main>
+
+<style>
+
+/* Main Screen Layout */
+.mainscreen {
+    min-height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--charcoal);
+    color: var(--charcoal)
+}
+
+/* Card Styling */
+.card {
+    width: 60rem;
+    margin: auto;
+    background: white;
+    border-radius: 1.5rem;
+    box-shadow: 4px 3px 20px #3535358c;
+    display: flex;
+    flex-direction: row;
+}
+
+/* Left Side Styling */
+.leftside {
+    width: 25rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-top-left-radius: 1.5rem;
+    border-bottom-left-radius: 1.5rem;
+}
+
+/* Product Image */
+.product {
+    object-fit: cover;
+    width: 20em;
+    height: 20em;
+    border-radius: 100%;
+}
+
+/* Right Side Styling */
+.rightside {
+    background-color: #ffffff;
+    width: 35rem;
+    border-bottom-right-radius: 1.5rem;
+    border-top-right-radius: 1.5rem;
+    padding: 1rem 2rem 3rem 3rem;
+}
+
+/* Paragraph Styling */
+p {
+    display: block;
+    font-size: 1.1rem;
+    font-weight: 400;
+    margin: .8rem 0;
+}
+
+/* Input Box Styling */
+.inputbox {
+    color: #030303;
+    width: 100%;
+    padding: 0.5rem;
+    border: none;
+    border-bottom: 1.5px solid #ccc;
+    margin-bottom: 1rem;
+    border-radius: 0.3rem;
+    font-family: var(--copy-font);
+    color: #615a5a;
+    font-size: 1.1rem;
+    font-weight: 500;
+    outline: none;
+}
+
+/* CVV Section Styling */
+.expcvv {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 0.6rem;
+}
+
+.expcvv_text {
+    padding-right: 1rem;
+}
+
+.expcvv_text2 {
+    padding: 0 1rem;
+}
+
+/* Button Styling */
+.button {
+    background: var(--candelLight);
+    color: var(--charcoal);
+    padding: 15px;
+    border: none;
+    border-radius: 50px;
+    font-weight: 400;
+    font-family: var(--copy-font);
+    font-size: 1.2rem;
+    margin-top: 10px;
+    width: 100%;
+    letter-spacing: .11rem;
+    outline: none;
+}
+
+/* Button Hover Effect */
+.button:hover {
+    transform: scale(1.05) translateY(-3px);
+    box-shadow: 3px 3px 6px #38373785;
+}
+
+/* Responsive Design */
+@media only screen and (max-width: 1000px) {
+    .card {
+        flex-direction: column;
+        width: auto;
+    }
+
+    .leftside {
+        width: 100%;
+        border-top-right-radius: 0;
+        border-bottom-left-radius: 0;
+        border-top-right-radius: 0;
+        border-radius: 0;
+    }
+
+    .rightside {
+        width: auto;
+        border-bottom-left-radius: 1.5rem;
+        padding: 0.5rem 3rem 3rem 2rem;
+        border-radius: 0;
+    }
+}
+
+
+/* Styling for Valid Input */
+.inputbox:valid {
+    border-bottom-color: green; /* Change border color on valid input */
+}
+
+</style>
