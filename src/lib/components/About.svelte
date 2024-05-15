da4d37faa9e1d7b7f2113dc3301e66366328d837
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2024-05-16 00:38:09 +0200

    :sparkles: Add about section and memoji

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,3 +1,77 @@
 <script>
 </script>
 
+<main>
+  <section class="hero">
+    <div class="container">
+      <div class="img--cover">
+        <img src=".\FG.png" alt="" />
+      </div>
+      <h1 class="hero--title">Hello, I'm Fatih</h1>
+      <p class="hero--desc">
+        I'm a 25-year-old front-end student based in
+        Amsterdam, North Holland, Netherlands. I am passionate about creating
+        beautiful and user-friendly web experiences. I am currently learning
+        HTML, CSS, JavaScript, and SvelteKit at the Amsterdam University of
+        Applied Science FDND. I am also interested in learning about other
+        front-end development technologies, such as React and Angular.
+      </p>
+    </div>
+  </section>
+</main>
+
+<style>
+  .container {
+    max-width: 1100px;
+    margin: auto;
+    padding: 0 1rem;
+  }
+
+  h1,
+  p {
+    color: var(--text-color);
+  }
+
+  section {
+    padding: 2rem 0;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  /* hero */
+
+  .hero .img--cover {
+    height: 150px;
+    width: 150px;
+    position: relative;
+    border-radius: 50%;
+    overflow: hidden;
+    margin: auto;
+  }
+
+  .hero--title {
+    text-align: center;
+    margin-top: 1rem;
+    font-size: 3rem;
+  }
+
+  .hero--desc {
+    text-align: center;
+    max-width: 700px;
+    margin: auto;
+    margin-top: 1rem;
+  }
+  /* responsive */
+  @media (max-width: 720px) {
+    .hero--title {
+      font-size: 2rem;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,5 +14,6 @@
 
 
 <style>
+  
 
 </style>
