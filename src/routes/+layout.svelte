3ca5bde749c2b00a6736aa41f9a1b681d1ec4c6b
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2024-12-25 02:41:12 +0100

    PUSH

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
     import '../lib/style/global.css'
+    import { injectAnalytics } from '@vercel/analytics/sveltekit'
     import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
+injectAnalytics
 injectSpeedInsights();
 </script>
 
